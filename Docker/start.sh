d49104c876a0dd19a6f4f1a4e6b66ce3ab4ecb75
Salvatore Cordiano <github@parallel.it>
2017-05-14 22:30:16 +0200

    Added -rm options to Docker run command to automatically remove the container when it exits

--- Docker/start.sh
+++ Docker/start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -it -p 80:3000 -v `pwd`/../:/drone-wall -w /drone-wall drone-wall npm start
+docker run -it -p 80:3000 -v `pwd`/../:/drone-wall -w /drone-wall --rm --name drone-wall drone-wall npm start
